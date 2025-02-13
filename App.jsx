7f3cfde6fb40cc3be6c1cf0fb340635c7ea7e2eb
sigallage <sgallage0000@gmail.com>
2025-02-13 09:59:56 +0530

    Update

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import Contact from "./Contact"
+import mrWhiskerson from "./images/mr-whiskerson.png"
+import fluffykins from "./images/fluffykins.png"
+import felix from "./images/felix.png"
+import pumpkin from "./images/pumpkin.png"
+
+
+function App() {
+    return (
+        <div className="contacts">
+            <Contact
+                img={mrWhiskerson}
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact
+                img={fluffykins}
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact
+                img={felix}
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img={pumpkin}
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
+        </div>
+    )
+}
+
+export default App
